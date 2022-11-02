42643af5e694ba77872d8d39dbaa0ceb063b2c92
Emmanuel Oladipupo <manueltech401@gmail.com>
2022-11-02 22:19:38 +0100

    Renamed from SquadDao to SquadRepository

--- src/main/java/com/decagon/scorecardapi/services/serviceimpl/SquadServiceImpl.java
+++ src/main/java/com/decagon/scorecardapi/services/serviceimpl/SquadServiceImpl.java
@@ -1,9 +1,8 @@
-package com.decagon.scorecardapi.services.impl;
+package com.decagon.scorecardapi.services.serviceimpl;
 
-import com.decagon.scorecardapi.dao.SquadDao;
+import com.decagon.scorecardapi.repository.SquadRepository;
 import com.decagon.scorecardapi.model.Squad;
 import com.decagon.scorecardapi.services.SquadService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -11,14 +10,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SquadServiceImpl implements SquadService {
-    private final SquadDao squadDao;
-    public SquadServiceImpl (SquadDao squadDao){
-        this.squadDao = squadDao;
+    private final SquadRepository squadRepository;
+    public SquadServiceImpl (SquadRepository squadRepository){
+        this.squadRepository = squadRepository;
     }
     @Override
     public Page<Squad> getAllSquads(int offset, int pageSize) {
         Pageable pageable = PageRequest.of(offset, pageSize);
-        Page<Squad> squadPage = squadDao.findAll(pageable);
-        return squadPage;
+        return squadRepository.findAll(pageable);
     }
 }
