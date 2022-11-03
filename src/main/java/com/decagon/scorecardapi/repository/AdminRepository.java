8233fe2033ed9be69f1fa82d711715b24bec927b
segunjs <segunnfayomi@gmail.com>
2022-11-03 02:15:58 +0100

    resolve merged conflict

--- src/main/java/com/decagon/scorecardapi/repository/AdminRepository.java
+++ src/main/java/com/decagon/scorecardapi/repository/AdminRepository.java
@@ -3,9 +3,6 @@ package com.decagon.scorecardapi.repository;
 import com.decagon.scorecardapi.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface AdminRepository extends JpaRepository<Admin, Long> {
 
-    List<Admin> findAll();
 }
