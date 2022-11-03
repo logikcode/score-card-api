e34201a4c7ba6649fdeff9f5301fff676913ccac
lim <hameed.korede.hk@gmail.com>
2022-11-03 21:51:09 +0100

    Implemented resolved conversation for implementing a squad endpoint

--- src/main/java/com/decagon/scorecardapi/controller/SuperAdminController.java
+++ src/main/java/com/decagon/scorecardapi/controller/SuperAdminController.java
@@ -3,6 +3,7 @@ package com.decagon.scorecardapi.controller;
 import com.decagon.scorecardapi.dto.responsedto.APIResponse;
 import com.decagon.scorecardapi.dto.responsedto.SquadDto;
 import com.decagon.scorecardapi.services.SuperAdminService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,13 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/super-admin")
+@RequiredArgsConstructor
 public class SuperAdminController {
 
     private final SuperAdminService superAdminService;
-    @Autowired
-    public SuperAdminController(SuperAdminService superAdminService) {
-        this.superAdminService = superAdminService;
-    }
+
     @PostMapping("/create-squad")
     public ResponseEntity<APIResponse<String>> createSquad (@RequestBody SquadDto squadDto) {
 
