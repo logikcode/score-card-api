d7cb0f89fed16d0b108c8e3a23e2ca10ff75aa3c
Emmanuel Oladipupo <manueltech401@gmail.com>
2022-11-03 08:52:55 +0100

    add @RequireArgsConstructor

--- src/main/java/com/decagon/scorecardapi/services/serviceimpl/SquadServiceImpl.java
+++ src/main/java/com/decagon/scorecardapi/services/serviceimpl/SquadServiceImpl.java
@@ -3,17 +3,15 @@ package com.decagon.scorecardapi.services.serviceimpl;
 import com.decagon.scorecardapi.repository.SquadRepository;
 import com.decagon.scorecardapi.model.Squad;
 import com.decagon.scorecardapi.services.SquadService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
+@RequiredArgsConstructor
 @Service
 public class SquadServiceImpl implements SquadService {
     private final SquadRepository squadRepository;
-    public SquadServiceImpl (SquadRepository squadRepository){
-        this.squadRepository = squadRepository;
-    }
     @Override
     public Page<Squad> getAllSquads(int offset, int pageSize) {
         Pageable pageable = PageRequest.of(offset, pageSize);
