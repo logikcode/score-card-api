023b99563013eb74b0fe09c0cb4870c056305301
Emmanuel Oladipupo <manueltech401@gmail.com>
2022-11-07 14:07:11 +0100

    reverted the s in the exception package import name

--- src/main/java/com/decagon/scorecardapi/exception/ResourceNotFoundException.java
+++ src/main/java/com/decagon/scorecardapi/exception/ResourceNotFoundException.java
@@ -1,13 +1,13 @@
-package com.decagon.scorecardapi.exception;
+package com.decagon.scorecardapi.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException{
-    private String resourceName;
-    private String fieldName;
-    private long fieldValue;
+    private final String resourceName;
+    private final String fieldName;
+    private final long fieldValue;
 
     public ResourceNotFoundException(String resourceName, String fieldName, long fieldValue) {
         super(String.format("%s not found %s : %s", resourceName, fieldName, fieldValue));
