8de032f180394b519c1e544dcf912917004635af
beny1122war <tyavbarnabas2@gmail.com>
2022-11-07 12:31:39 +0100

    removed duplicate depencies

--- src/main/java/com/decagon/scorecardapi/service/StackService.java
+++ src/main/java/com/decagon/scorecardapi/service/StackService.java
@@ -2,12 +2,10 @@ package com.decagon.scorecardapi.service;
 
 import com.decagon.scorecardapi.dto.StackDto;
 import com.decagon.scorecardapi.dto.responsedto.APIResponse;
-import com.decagon.scorecardapi.model.Stack;
 import com.decagon.scorecardapi.model.StackTemplate;
 
 public interface StackService {
 
       APIResponse <StackTemplate> createStack(StackDto stackDto);
 
-//      boolean checkStack(String stackName);
 }
