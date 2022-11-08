84edf2ee9977f4afbbf00764802762f2b5ef750a
danielkpobari <danielkpobari@gmail.com>
2022-11-08 12:13:31 +0100

    merged_conflict

--- src/main/java/com/decagon/scorecardapi/model/Admin.java
+++ src/main/java/com/decagon/scorecardapi/model/Admin.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.*;
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -30,7 +29,6 @@ public class Admin extends User{
     @ManyToMany
     @JoinColumn(name = "admin_pod",referencedColumnName = "id")
     private List<Pod> pods;
-
     public void setActive(boolean b) {
     }
 }
--- src/main/java/com/decagon/scorecardapi/serviceImpl/SuperAdminServiceImpl.java
+++ src/main/java/com/decagon/scorecardapi/serviceImpl/SuperAdminServiceImpl.java
@@ -29,7 +29,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 
