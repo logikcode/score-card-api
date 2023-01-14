b47e185cdf474e9404b21d93873b16f282aebbb9
Emmanuel Oladipupo <emmanuel.oladipupo@decagon.dev>
2023-01-14 13:51:10 +0100

    add method for retrieving from the system Decadev through Id

--- src/main/java/com/decagon/scorecardapi/repository/DecadevRepository.java
+++ src/main/java/com/decagon/scorecardapi/repository/DecadevRepository.java
@@ -1,7 +1,11 @@
 package com.decagon.scorecardapi.repository;
 
 import com.decagon.scorecardapi.model.Decadev;
+import com.decagon.scorecardapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DecadevRepository extends JpaRepository<Decadev, Long> {
+    // by Emmanuel
+    Decadev findByDecadevId(String devId);
+
 }
